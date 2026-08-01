0013e55f18e502088ea897812fca043833f3e94c
alok-666 <alokmalik8261@gmail.com>
2026-08-01 17:32:29 +0530

    Add files via upload

---
+++ Basic of c++/Operators/Arithmatic Operators/subtract.cpp
@@ -0,0 +1,16 @@
+//subtract of numbers
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int price;
+    float newprice;
+    cout <<"Enter Your Price:";
+    cin >>price;
+    cout <<"Enter Your newprice:";
+    cin >>newprice;
+    cout<<price-newprice;
+
+    return 0;
+}
