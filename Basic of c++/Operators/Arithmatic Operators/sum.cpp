03d43608fd12e687274cd67656058ace5e229f4a
alok-666 <alokmalik8261@gmail.com>
2026-08-02 11:23:28 +0530

    Add files via upload

---
+++ Basic of c++/Operators/Arithmatic Operators/sum.cpp
@@ -0,0 +1,18 @@
+//sum of numbers
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int Englishmark;
+    int Hindimark;
+    cout<<"Enter your English mark: ";
+    cin>>Englishmark;
+    cout<<"Enter your Hindi Mark: ";
+    cin>> Hindimark;
+    cout <<"Your Total Mark: "<< Englishmark + Hindimark;
+    return 0;
+}
+
+
+
---
+++ Basic of c++/Operators/Relational Operators/compairision.cpp
@@ -0,0 +1,13 @@
+//compairision of numbers
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout<<(7>5)<<endl;//for true print 1 for false print 0
+    cout<<(67==76)<<"\n";
+    cout<<(65<786)<<"\n";
+    cout<<(6587!=65)<<"\n";
+    cout<<(65<=5)<<"\n";
+    cout<<(686>=78);
+    return 0;
+}
